107543e65f963ed97e8e518f2be114d10444bd2d
Thibtoy <cabanes.thibault@gmail.com>
2020-04-27 12:42:59 +0200

    front

--- Application/src/dist/index.js
+++ Application/src/dist/index.js
@@ -4,16 +4,6 @@ import Modale from './Modale.js';
 import Yggdrasil from './Yggdrasil/Yggdrasil.js';
 
 class App extends Yggdrasil{
-	constructor() {
-		super();
-		this.ancre = {element: document.createElement('div'), append: function() {
-			this.element.setAttribute('key', Date.now());
-			return this.element;
-		}};
-		this.ancre.element.id = 'register';
-		this.ancre.element.innerHTML = 'register';
-		this.router.link(this.ancre.element);
-	}
 }
 
 const app = new App();
